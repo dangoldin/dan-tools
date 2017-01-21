8fa00d6880e45f87c2229219ac721bd8a7e3cbe7
Dan Goldin <dangoldin@gmail.com>
2017-01-20 22:07:31 -0700

    Start working on react version. Based on https://medium.com/@Agro/developing-desktop-applications-with-electron-and-react-40d117d97564\#.7mdc7udx6

---
+++ javascripts/entry.js
@@ -0,0 +1,8 @@
+require('../less/main.less');
+
+'use strict';
+
+import React from "react";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div className="myDiv">Hello Electron!</div>, document.getElementById('content'));
