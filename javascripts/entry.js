44100f63d55ea9afd62b8faa020bc54c597d39d3
Dan Goldin <dangoldin@gmail.com>
2017-01-21 23:26:11 -0500

    More messing around

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -5,4 +5,24 @@ require('../less/main.less');
 import React from "react";
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div className="myDiv">Hello Electron!</div>, document.getElementById('content'));
+function Welcome(props) {
+  return (
+      <div>
+        <h1>Hello, {props.name}</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Dan's Tools" />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('content')
+);
