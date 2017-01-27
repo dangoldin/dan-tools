644fabbb424cb93fedf74e1a525e0e11aa2b6734
Dan Goldin <dangoldin@gmail.com>
2017-01-26 22:37:05 -0500

    Start working on routing

---
+++ javascripts/bulk-geocoding.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from "react";
+
+class BulkGeocoding extends React.Component {
+  render() {
+        return (
+          <h1>Bulk Geocoding</h1>
+        )
+  }
+}
+
+export default BulkGeocoding
--- javascripts/entry.js
+++ javascripts/entry.js
@@ -4,8 +4,10 @@ require('../less/main.less');
 
 import React from "react";
 import ReactDOM from 'react-dom';
+// import { Router, Route, Link, browserHistory } from 'react-router'
 
 import ConvertCSVtoBootstrapTable from './convert-csv-to-bootstrap-table';
+import BulkGeocoding from './bulk-geocoding';
 
 const {ipcRenderer} = require('electron')
 
@@ -14,25 +16,43 @@ ipcRenderer.on('global-shortcut', (event, arg) => {
     console.log(arg)
 })
 
-function App() {
-  return (
-    <div>
-      <div>
-        <h1>
-            Welcome to Dan's Tools
-        </h1>
-      </div>
-      <ul>
-        <li>CSV to Bootstrap Table</li>
-        <li>Bulk Geocoding</li>
-        <li>"BCG Style" Matrix</li>
-        <li>SQL Data Generation</li>
-        <li>SQL Schema Comparison</li>
-        <li>Date Generation</li>
-      </ul>
-      <ConvertCSVtoBootstrapTable />
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            page: 'showCSV'
+        }
+    }
+
+    render() {
+        let Page
+        switch (this.state.page) {
+            case 'showCsv': Page = ConvertCSVtoBootstrapTable; break;
+            case 'bulkGeocoding': Page = BulkGeocoding; break;
+            default: Page = ConvertCSVtoBootstrapTable; break;
+        }
+
+        return (
+            <div>
+            <div>
+                <h1>
+                    Welcome to Dan's Tools
+                </h1>
+            </div>
+            <div className="nav">
+                <ul>
+                    <li><a href="#">CSV to Bootstrap Table</a></li>
+                    <li><a href="#">Bulk Geocoding</a></li>
+                    <li><a href="#">"BCG Style" Matrix</a></li>
+                    <li><a href="#">SQL Data Generation</a></li>
+                    <li><a href="#">SQL Schema Comparison</a></li>
+                    <li><a href="#">Date Generation</a></li>
+                </ul>
+            </div>
+            <Page/>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
